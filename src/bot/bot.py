377b95be8ef3bd09b587e95300c1cbed53771ed8
Cherithe <84004098+Cherithe@users.noreply.github.com>
2021-12-14 11:46:00 +1100

    Adding basic file structure for src files.

---
+++ src/bot/bot.py
@@ -0,0 +1,15 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'A connection between {client.user} and Discord has been created.')
+
+client.run(TOKEN)
