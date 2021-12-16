e9fa610eef0225fb3d3d82d734a38b5e03d30255
Cherithe <84004098+Cherithe@users.noreply.github.com>
2021-12-16 12:42:38 +1100

    Modified file structure to use the cogs functionality.

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,22 +1,40 @@
+"""This file contains the main framework for the Discord Bot to function."""
+
 import os
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-import random
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-help_command = commands.DefaultHelpCommand(
-    no_category = 'Commands'
-)
 intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='!', intents=intents, help_command = help_command)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# The words from BannedWords.txt which contain profanity and slurs to be
+# filtered out.
+with open('BannedWords.txt', 'r') as f:
+    global banned_words
+    banned_words = f.read().split()
+
+for filename in os.listdir('./cogs'):
+  if filename.endswith('.py'):
+    bot.load_extension(f'cogs.{filename[:-3]}')
 
 @bot.event
 async def on_ready():
+    """This function prints information out to standard output when
+    the bot has established a connection to Discord.
+
+    Arguments:
+        - None
+    
+    Return Value:
+        - None
+    """
+
     for guild in bot.guilds:
         if guild.name == GUILD:
             break
@@ -28,6 +46,16 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
+    """This function sends a welcome message to members when they
+    join a guild that the bot is currently active on.
+
+    Arguments:
+        - member (object): An object created under the Member class in Discord API.
+    
+    Return Value:
+        - None
+    """
+
     await member.create_dm()
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to my rad Discord server!\n'
@@ -35,19 +63,11 @@ async def on_member_join(member):
 
 @bot.event
 async def on_message(message):
-    # Add stuff here - Message listener
+    msg = message.content
+    for word in banned_words:
+        if word in msg:
+            await message.delete()
+            await message.channel.send("Dont use that word!")
     await bot.process_commands(message)
-
-@bot.command(name='coinflip', help='I mean... its a coinflip.')
-async def flip(ctx):
-    dice = ['The coin landed on heads!', 'The coin landed on tails!']
-    response = random.choice(dice)
-    await ctx.send(response)
-
-@bot.command(name='rate', help='Rates literally anything.')
-async def rate(ctx, subject):
-    rating = str(random.choice(range(0, 11)))
-    response = f'I give {subject} a {rating}/10.'
-    await ctx.send(response)
 
 bot.run(TOKEN)
---
+++ src/bot/cogs/general.py
@@ -0,0 +1,23 @@
+"""This file contains the cog for general commands."""
+
+from discord.ext import commands
+import random
+
+class General(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='coinflip', help='I mean... its a coinflip.')
+    async def coinflip(self, ctx):
+        dice = ['The coin landed on heads!', 'The coin landed on tails!']
+        response = random.choice(dice)
+        await ctx.send(response)
+
+    @commands.command(name='rate', help='Rates literally anything out of 10.')
+    async def rate(self, ctx, subject):
+        rating = str(random.choice(range(0, 11)))
+        response = f'I give {subject} a {rating}/10.'
+        await ctx.send(response)
+
+def setup(bot):
+    bot.add_cog(General(bot))
