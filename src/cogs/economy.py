b5773a61a4e597f4db0f322a5afe426df6ced821
Cherithe <84004098+Cherithe@users.noreply.github.com>
2021-12-18 16:40:53 +1100

    Added halfworking daily command.

--- src/cogs/economy.py
+++ src/cogs/economy.py
@@ -1,11 +1,13 @@
 """This file contains the cog for economy commands."""
 
-from datetime import datetime
+import datetime
+from datetime import datetime, timedelta
 from discord.ext import commands
 
 from datastore import data_store
 
 def find_user(user_id):
+    print("HI")
     data = data_store.get()
     users = data['users']
     user = next((user for user in users
@@ -15,7 +17,7 @@ def find_user(user_id):
             'user_id': user_id,
             'money': 0,
             'streak': 0,
-            'last_claim': datetime(),
+            'last_claim': str(datetime.min.timestamp()),
         })
         data_store.set(data)
     return {'data': data, 'user': user}
@@ -28,8 +30,23 @@ class Economy(commands.Cog):
     async def daily(self, ctx):
         data = find_user(ctx.message.author.id)
         user = data['user']
+        now = datetime.now()
+        delta = now - datetime.fromtimestamp(float(data['last_claim']))
+        if delta < timedelta(hours=24):
+            await ctx.send('You already claimed your daily allowance for '
+                            'today. Be more patient!')
+            return
 
-        data_store.set(data)
+        if delta < timedelta(hours=48):
+            user['streak'] += 1
+        else:
+            user['streak'] = 0
+        money = 50 * (1 + 0.2 * user['streak'])
+        user['money'] += money
+        response = f'Thanks for stopping by. {user.mention} received {money} coins today.'
+        if user['streak'] > 0:
+            response += f'\nThey are on a {user["streak"]} day streak!'
+        data_store.set(data['data'])
 
 def setup(bot):
     bot.add_cog(Economy(bot))
